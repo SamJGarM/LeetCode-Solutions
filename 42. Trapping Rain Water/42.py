0579119b3e4cb50335060acd9d5bc719f4fe7a28
Samuel Jaden García Muñoz <samjgarm@gmail.com>
2025-08-21 20:36:29 +0100

    Solution for 42 (and improved solution for better space complexity).

---
+++ 42. Trapping Rain Water/42.py
@@ -0,0 +1,41 @@
+"""
+Author: Samuel Jaden García Muñoz
+Date: 21/08/2025
+Note:-
+
+"""
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # Array for checking the max in any given location from the left.
+        leftMax = [0] * len(height)
+        # ... from the right.
+        rightMax = [0] * len(height)
+
+        # Iterate through the entire array and store the current max height from the left.
+        for i in range(len(height)):
+            if i == 0:
+                leftMax[i] = height[i]
+            else:
+                leftMax[i] = max(height[i], leftMax[i - 1])
+        
+        # Iterate through the entire array and store the current max height from the right.
+        for i in range(len(height) - 1, -1, -1):
+            if i == len(height) - 1:
+                rightMax[i] = height[len(height) - 1]
+            else:
+                rightMax[i] = max(height[i], rightMax[i + 1])
+
+        # Run through the array one last time, calculating the max difference in height at any given position,
+        # only taking values greater than 0. (as we can't trap negative water)
+        res = 0
+        for i in range(len(height)):
+            res += max(0, min(leftMax[i], rightMax[i]) - height[i])
+                
+        # Return the result.
+        return res
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
---
+++ 42. Trapping Rain Water/42_improved.py
@@ -0,0 +1,44 @@
+"""
+Author: Samuel Jaden García Muñoz
+Date: 21/08/2025
+Note:-
+Improved approach with 2 pointers to reduce the space complexity.
+"""
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # Left and right pointer for tracking our position through the array.
+        left, right = 0, len(height) - 1
+        # Left and right maximum variables for tracking the edges for trapping water.
+        leftMax, rightMax = height[left], height[right]
+
+        # Variable for keeping a running result of water we're trapping.
+        res = 0
+
+        # Iterate until the pointers meet.
+        while left <= right:
+            # Proceeding with the smallest maximum.
+            if leftMax < rightMax:
+                # Check if we've reached a new maximum.
+                if height[left] > leftMax:
+                    leftMax = height[left]
+                # If we haven't, add the amount of water we can collect (or 0 in the case of a negative value).
+                else:
+                    res += max(leftMax - height[left], 0)
+                # Increase the left pointer.
+                left += 1
+            # Repeat the process if the rightMax is the smaller maximum.
+            else:
+                if height[right] > rightMax:
+                    rightMax = height[right]
+                else:
+                    res += max(rightMax - height[right], 0)
+                right -= 1
+        
+        # Return the result.
+        return res
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
