ab1244dbae260d3b0bb4f80ce2760ebe4dee05a2
Samuel Jaden García Muñoz <samjgarm@gmail.com>
2025-08-22 21:54:35 +0100

    Solution for 133.

---
+++ 133. Clone Graph/133.py
@@ -0,0 +1,46 @@
+"""
+Author: Samuel Jaden García Muñoz
+Date: 22/08/2025
+Note:-
+Should try a BFS version as well.
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # Dictionary for mapping nodes to their corresponding clone.
+        cloneMap = {}
+
+        # Conduct a depth-first search from a given node.
+        def dfs(node):
+            # If the node is already in the clone map, we can return the cloned node.
+            if node in cloneMap:
+                return cloneMap[node]
+
+            # Otherwise, create a copy of the current node, and store it in the map.
+            copy = Node(node.val)
+            cloneMap[node] = copy
+
+            # Iterate through each neighbor in the current node.
+            for neighbor in node.neighbors:
+                # Add the clone of the neighbor to the neighbors list of the current node.
+                copy.neighbors.append(dfs(neighbor))
+            
+            # Return the clone.
+            return copy
+            
+        # If node isn't null, return the "head" of the clone graph, otherwise return null.
+        return dfs(node) if node else None
+
+"""
+Time Complexity: O(V + E)
+Space Complexity: O(V)
+"""
